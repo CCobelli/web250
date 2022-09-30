889c0aac9151616c3e595ea12bd1aa0531b2db05
Calvin <cobelli57@gmail.com>
2022-09-30 17:38:54 -0400

    Started the asgn04 bird challenge.

---
+++ asgn04-bird-challenge/public/bird.php
@@ -0,0 +1,50 @@
+<?php require_once('../private/initialize.php'); ?>
+
+<?php $page_title = 'Inventory'; ?>
+<?php include(SHARED_PATH . '/public_header.php'); ?>
+
+<div id="main">
+
+  <div id="page">
+    <div class="intro">
+      <h1>Small Sampling of WNC Birds</h1>
+    </div>
+
+    <table id="inventory">
+      <tr>
+        <th>Common Name</th>
+        <th>Habitat</th>
+        <th>Food</th>
+        <th>Nest Placement</th>
+        <th>Behavior</th>
+        <th>Conservations Level</th>
+        <th>Backyard Tips</th>
+      </tr>
+
+      <?php 
+
+      $parser = new ParseCSV(PRIVATE_PATH . '/used_bicycles.csv');
+      $bike_array = $parser->parse();
+
+      ?>
+
+      <?php foreach($bike_array as $args) { ?>
+        <?php $bike = new Bicycle($args); ?>
+      <tr>
+        <td><?php echo h($bike->brand); ?></td>
+        <td><?php echo h($bike->model); ?></td>
+        <td><?php echo h($bike->year); ?></td>
+        <td><?php echo h($bike->category); ?></td>
+        <td><?php echo h($bike->gender); ?></td>
+        <td><?php echo h($bike->color); ?></td>
+        <td><?php echo h($bike->weight_kg()) . ' / ' . h($bike->weight_lbs()); ?></td>
+        <td><?php echo h($bike->condition()); ?></td>
+        <td><?php echo h($bike->price); ?></td>
+      </tr>
+      <?php } ?>
+
+    </table>
+  </div>
+
+</div>
+
