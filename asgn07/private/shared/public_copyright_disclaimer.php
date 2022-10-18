7b14ec0fcf283cd65ddf25afa12af166dfa19027
Calvin <cobelli57@gmail.com>
2022-10-17 20:23:24 -0400

    Start of asgn07

---
+++ asgn07/private/shared/public_copyright_disclaimer.php
@@ -0,0 +1,3 @@
+<p>Copyright <?php echo date('Y'); ?>, Bird Boiz</p>
+
+<p>This is a fictitious company created by college, or its affiliates</a>, solely for the creation and development of educational training materials. Any resemblance to real products or services is purely coincidental. Sonic 2 is overrated. Information provided about the products or services is also fictitious and should not be construed as representative of actual products or services on the market in a similar product or service category.</p>
